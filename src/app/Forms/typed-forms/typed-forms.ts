4b7ac51cabdf5fe80476db034972f62ab729ba3a
Guru Prakash <10187004+guruprakash-c@users.noreply.github.com>
2025-09-13 21:34:18 +0530

    Resolved issue with untyped forms

--- src/app/Forms/typed-forms/typed-forms.ts
+++ src/app/Forms/typed-forms/typed-forms.ts
@@ -1,20 +1,22 @@
 import { Component, Injectable } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, FormsModule } from '@angular/forms';
+import { UntypedFormControl, UntypedFormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-typed-forms',
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './typed-forms.html',
   styleUrl: './typed-forms.scss'
 })
 export class TypedFormsComponent {
-  register = new UntypedFormGroup({
-    fullName: new UntypedFormControl('DEVstoc'),
-    email: new UntypedFormControl('hi@devstoc.com',{ nonNullable: true})
+  register:UntypedFormGroup = new UntypedFormGroup({
+    fullName: new UntypedFormControl(''),
+    email: new UntypedFormControl('',{ nonNullable: true})
   });
   OnSubmit(){
+    if(!this.register.valid) return;
     const fName = this.register.controls['fullName'].value;
     const eMail = this.register.controls['email'].value; 
     alert(`Name: ${fName} & e-Mail: ${eMail}`);
+    this.register.reset();
   }
 }
